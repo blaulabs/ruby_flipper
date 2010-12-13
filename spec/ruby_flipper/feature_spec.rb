7bad5cc23cf69a7ff7bd9bc24337914b0cd8fc3f
Thomas Jachmann <self@thomasjachmann.com>
2010-12-13 11:59:57 +0100

    feature.condition_met? specs

--- spec/ruby_flipper/feature_spec.rb
+++ spec/ruby_flipper/feature_spec.rb
@@ -58,6 +58,55 @@ describe RubyFlipper::Feature do
 
   end
 
+  describe '.condition_met?' do
+
+    context 'with a symbol' do
+
+      it 'should return the state of the referenced feature' do
+        RubyFlipper::Feature.add(:referenced, true)
+        RubyFlipper::Feature.condition_met?(:referenced).should == true
+      end
+
+      it 'should raise an error when the referenced feature is not defined' do
+        lambda { RubyFlipper::Feature.condition_met?(:missing) }.should raise_error RubyFlipper::NotDefinedError, '\'missing\' is not defined'
+      end
+
+    end
+
+    {
+      true       => true,
+      'anything' => true,
+      false      => false,
+      nil        => false
+    }.each do |condition, expected|
+
+      it "should call a given proc and return #{expected} when it returns #{condition}" do
+        RubyFlipper::Feature.condition_met?(lambda { condition }).should == expected
+      end
+
+      it "should call anything callable and return #{expected} when it returns #{condition}" do
+        RubyFlipper::Feature.condition_met?(stub(:call => condition)).should == expected
+      end
+
+      it "should return #{expected} when the condition is #{condition}" do
+        RubyFlipper::Feature.condition_met?(condition).should == expected
+      end
+
+    end
+
+    context 'with a complex proc' do
+
+      it 'should return the met? of the combined referenced conditions' do
+        RubyFlipper::Feature.add(:true, true)
+        RubyFlipper::Feature.add(:false, false)
+        RubyFlipper::Feature.condition_met?(lambda { active?(:true) || active?(:false) }).should == true
+        RubyFlipper::Feature.condition_met?(lambda { active?(:true) && active?(:false) }).should == false
+      end
+
+    end
+
+  end
+
   describe 'initializer' do
 
     it 'should store the name' do
@@ -118,55 +167,6 @@ describe RubyFlipper::Feature do
 
     it 'should return true when all conditions are met' do
       RubyFlipper::Feature.new(:feature_name, true, true).active?.should == true
-    end
-
-  end
-
-  describe '.condition_met?' do
-
-    context 'with a symbol' do
-
-      it 'should return the active? of the referenced feature' do
-        RubyFlipper::Feature.add(:referenced, true)
-        RubyFlipper::Feature.condition_met?(:referenced).should == true
-      end
-
-      it 'should raise an error when the referenced feature is not defined' do
-        lambda { RubyFlipper::Feature.condition_met?(:missing) }.should raise_error RubyFlipper::NotDefinedError, '\'missing\' is not defined'
-      end
-
-    end
-
-    {
-      true       => true,
-      'anything' => true,
-      false      => false,
-      nil        => false
-    }.each do |condition, expected|
-
-      it "should call a given proc and return #{expected} when it returns #{condition}" do
-        RubyFlipper::Feature.condition_met?(lambda { condition }).should == expected
-      end
-
-      it "should call anything callable and return #{expected} when it returns #{condition}" do
-        RubyFlipper::Feature.condition_met?(stub(:call => condition)).should == expected
-      end
-
-      it "should return #{expected} when the condition is #{condition}" do
-        RubyFlipper::Feature.condition_met?(condition).should == expected
-      end
-
-    end
-
-    context 'with a complex proc' do
-
-      it 'should return the met? of the combined referenced conditions' do
-        RubyFlipper::Feature.add(:true, true)
-        RubyFlipper::Feature.add(:false, false)
-        RubyFlipper::Feature.condition_met?(lambda { active?(:true) || active?(:false) }).should == true
-        RubyFlipper::Feature.condition_met?(lambda { active?(:true) && active?(:false) }).should == false
-      end
-
     end
 
   end
