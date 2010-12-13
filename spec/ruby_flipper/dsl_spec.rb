ce8ceebebe508314ccbf2030e028131615591844
Thomas Jachmann <self@thomasjachmann.com>
2010-12-13 11:31:11 +0100

    better dsl specs

--- spec/ruby_flipper/dsl_spec.rb
+++ spec/ruby_flipper/dsl_spec.rb
@@ -2,27 +2,31 @@ require 'spec_helper'
 
 describe RubyFlipper::Dsl do
 
-  describe '#condition' do
-
-    it 'should store a condition' do
-      block = lambda { true }
-      condition = subject.condition :condition_name, true, false, &block
-      condition.should be_a(RubyFlipper::Feature)
-      condition.conditions.should == [true, false, block]
-      RubyFlipper::Feature.find(:condition_name).should == condition
-    end
-
+  it 'should ensure mocked methods exist' do
+    RubyFlipper::Feature.should respond_to(:add)
   end
 
-  describe '#feature' do
+  [:feature, :condition].each do |method|
 
-    it 'should store a condition' do
-      feature = subject.feature :feature_name, :description => 'desc', :condition => :cond
-      feature.should be_a(RubyFlipper::Feature)
-      feature.name.should == :feature_name
-      feature.description.should == 'desc'
-      feature.conditions.should == [:cond]
-      RubyFlipper::Feature.find(:feature_name).should == feature
+    describe "##{method}" do
+
+      it 'should call add with only a name' do
+        RubyFlipper::Feature.expects(:add).with(:feature_name).returns(:feature)
+        subject.send(method, :feature_name).should == :feature
+      end
+
+      it 'should call add with a name and conditions' do
+        RubyFlipper::Feature.expects(:add).with(:feature_name, :one, :two).returns(:feature)
+        subject.send(method, :feature_name, :one, :two).should == :feature
+      end
+
+      it 'should call add with a name, conditions and a block' do
+        block = lambda {}
+        # cannot mock this since mocha doesn't allow to test for a proc [thomas, 2010-12-13]
+        feature = subject.send(method, :feature_name, :one, :two, &block)
+        feature.conditions.should == [:one, :two, block]
+      end
+
     end
 
   end
